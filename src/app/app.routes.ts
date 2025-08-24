b700640c9ee788a16a136dece6884e6e78996962
Allan Chan <allanchan@cpp.edu>
2025-08-23 17:27:22 -0700

    Added page files and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { Messages } from './pages/messages/messages';
+import { Friends } from './pages/friends/friends';
+import { Groups } from './pages/groups/groups';
+
+export const routes: Routes = [
+  { path: 'home', component: Home },
+  { path: 'messages', component: Messages },
+  { path: 'friends', component: Friends },
+  { path: 'groups', component: Groups },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // default
+  { path: '**', redirectTo: '/home' } // fallback
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
